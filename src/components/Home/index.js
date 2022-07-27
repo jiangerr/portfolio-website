91b5695daee3580750c191af97f9c50369d2adfe
Jianger <ethantianshujiang@gmail.com>
2022-07-26 19:37:32 -0600

    minor changes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import Masonry from 'react-masonry-css';
 import Photos from '../Photos/Home.json';
 // import { Link } from 'react-router-dom';
 import './index.scss';
-
+import React, { useState, useCallback, stopPropagation } from 'react';
 const Columns = {
     default: 4,
     1500: 3,
@@ -12,29 +12,47 @@ const Columns = {
 };
 
 const Home = () => {
+    const [currentImg, setCurrentImg] = useState(0);
+    const [viewerOpen, setViewerOpen] = useState(false);
+
+    const openViewer = useCallback((photo) => {
+        setCurrentImg(photo);
+        setViewerOpen(true);
+        // console.log(`test ${photo.id}`);
+    });
+
+    const closeViewer = () => {
+        setCurrentImg(0);
+        setViewerOpen(false);
+        console.log('closed');
+    };
+
+    const stopChildClick = (e) => {
+        e.stopImmediatePropagation();
+        console.log('stop');
+    };
+
     return (
-        <div className="container home-page">
-            <Masonry breakpointCols={Columns} className="masonry-grid" columnClassName='masonry-grid_column'>
-                {Photos.map(({ id, src, alt }) => {
-                    return (
-                        <div key={id} className="masonry-grid-column">
-                            <img src={src} alt={alt} width="90%"/>
-                        </div>
-                    );
-                })}
-            </Masonry>
-            {/*<div className="text-zone">
-                <h1>
-                    <img src={LogoEJ} alt="Ethan Jiang" />
-                    <br />
-                    WELCOME.
-                    <br />
-                    Ethan Jiang
-                    <br />
-                </h1>
-                <h2>roles, etc. FILL IN LATER</h2>
-                <Link to="/contact" className="flat-button">Contact Me</Link>
-            </div> */}
+        <div className="container">
+            {viewerOpen ?
+                <div className='image-viewer-background' onClick={closeViewer}>
+                    <div className='image-viewer' onClick={() => console.log('help')}>
+                        <h1>TEST</h1>
+                        {currentImg}
+                    </div>
+                </div>
+                :
+                <div className="home-page">
+                    <Masonry breakpointCols={Columns} className="masonry-grid" columnClassName='masonry-grid_column'>
+                        {Photos.map(({ id, src, alt }) => {
+                            return (
+                                <div key={id} className="masonry-grid-column">
+                                    <img src={src} alt={alt} onClick={openViewer} width="90%" />
+                                </div>
+                            );
+                        })}
+                    </Masonry>
+                </div>}
         </div>
     );
 }
