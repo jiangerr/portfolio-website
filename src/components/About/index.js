2f19b1f46ef19a339a8e8759de7929459e191afb
Jianger <ethantianshujiang@gmail.com>
2022-08-06 02:56:42 -0600

    fixed about page flexbox, updated homepage gutter sizes to be size-adaptive

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,17 +7,21 @@ const About = () => {
             <div className='toptext-zone'>
                 <h1>NICE TO MEET YOU.</h1>
             </div>
-            <div className='picture-zone'>
-                <img src={AboutPicture} alt='Ethan Jiang' />
-            </div>
-            <div className='text-zone'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                    pariatur. Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className='flex-zone-wrapper'>
+                <div className='flex-zone'>
+                    <div className='picture-zone'>
+                        <img src={AboutPicture} alt='Ethan Jiang' />
+                    </div>
+                    <div className='text-zone'>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident,
+                            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
